af0a5354b1b51844a4515109284ce36db2e0ecec
anton-isupov <tony.isupov2010@yandex.ru>
2020-02-16 18:57:22 +0300

    start angular app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,29 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginPageComponent} from './pages/login.page/login.page.component';
+import {PageNotFoundComponent} from './pages/pagenotfound/pagenotfound.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: 'login',      component:  LoginComponent},
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    LoginPageComponent,
+    PageNotFoundComponent,
+    NavbarComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes
+    ),
     BrowserModule
   ],
   providers: [],
